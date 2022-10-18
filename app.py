7ddd378d4bab5495f53c09a6b3d2e5afed6d313b
julia <j.v.denisowa@gmail.com>
2022-10-19 02:13:08 +0700

    Created templates (base.html, head_image.html, navbar.html, footer.html, index.html) and updated app.py (added menu, updated index).

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+menu = [
+    {"name": "Блог", "url": "/"},
+    {"name": "Подкаст", "url": "/podcast"},
+    {"name": "Сообщество", "url": "/community"},
+]
+
+
 @app.route("/")
 def index():
-    return "test"
+    return render_template("index.html", menu=menu, title="Блог")
 
 
 if __name__ == "__main__":
