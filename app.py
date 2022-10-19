674e0b67ef47868344a2e9156cead1ace8bf4bdd
julia <j.v.denisowa@gmail.com>
2022-10-19 14:19:26 +0700

    Updated templates (added podcast.html and community.html) and app.py (created podcast and community).

--- app.py
+++ app.py
@@ -15,5 +15,15 @@ def index():
     return render_template("index.html", menu=menu, title="Блог")
 
 
+@app.route("/podcast")
+def podcast():
+    return render_template('podcast.html', menu=menu, title="Подкаст")
+
+
+@app.route("/community")
+def community():
+    return render_template('community.html', menu=menu, title="Сообщество")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
